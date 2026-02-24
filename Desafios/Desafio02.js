c9e5a627016d87f8d1a257396d720dc7d7a1f775
tiago-burlamaque <tiagoburlamaque79@gmail.com>
2026-02-24 01:07:27 -0300

    Desafio 2 finalizado

--- Desafios/Desafio02.js
+++ Desafios/Desafio02.js
@@ -1,10 +1,10 @@
-const taz = "Taz"
-const gangue = []
+
 
 function criarTazHeArray() {
+    let gangue = ["taz"]
 
-    if (gangue == "") {
-        gangue.push(taz)
+    if (gangue.length === 0) {
+        gangue.push(gangue[0].toLowerCase())
     }
 
     console.log(gangue)
@@ -12,13 +12,58 @@ function criarTazHeArray() {
 
 criarTazHeArray()
 
+
+
 function removerPersonagem() {
-  
-    if (gangue.length[-1] ==! taz) {
-        gangue.pop()
-    } else {
-        return console.log("Taz não pode ser tirado da gangue!");
+    let gangue = ["taz", "ronaldo"]
+    
+    if(gangue.length > 0) {
+        let ultimo = gangue[gangue.length - 1]
+
+        if(ultimo !== "Taz") {
+            gangue.pop()
+        }
     }
+    console.log(gangue)
 }
 
 removerPersonagem()
+
+function removerSeMenorQue5() {
+    let gangue = ["Taz", "Pernalonga", "Lola"];
+
+    if (gangue.length > 0 && gangue[0].length < 5) {
+        gangue.shift();
+    }
+
+    console.log(gangue);
+}
+
+removerSeMenorQue5();
+
+function AdicionarTina() {
+    let gangue = ["Taz", "Pernalonga", "Lola"];
+
+    
+    if(gangue[2][3] === "a") {
+        gangue.push("Tina".toLowerCase())
+    }
+    console.log(gangue);
+    
+}
+
+AdicionarTina()
+
+function copiaLimitada() {
+    let gangue = ["Taz", "Lola", "Pateta", "Mickey", "Batman", "Robin", "Tiago"];
+
+
+    let filtrados = gangue.filter(nome => 
+        nome.length === 5 || nome.length === 8
+    )
+
+    console.log(filtrados);
+    
+}
+
+copiaLimitada()
